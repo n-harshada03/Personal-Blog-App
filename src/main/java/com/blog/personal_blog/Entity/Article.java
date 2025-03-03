b83173720907043801d9d0b22d3cf7a63959b705
n-harshada03 <harshdanavale2003@gmail.com>
2025-03-03 20:12:30 +0530

    entity classes and repository

--- src/main/java/com/blog/personal_blog/Entity/Article.java
+++ src/main/java/com/blog/personal_blog/Entity/Article.java
@@ -1,7 +1,36 @@
 package com.blog.personal_blog.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private int articleId;
+
+    @Column(name = "article_title",nullable = false)
+    private String articleTitle;
+
+    @Column(name = "publish_date",nullable = false)
+    private LocalDateTime publishDate;
+
+    @Column(name = "content",nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private AppUser appUser;
+
 }
